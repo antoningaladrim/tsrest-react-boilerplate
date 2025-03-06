8c2988068b508a759dec8b042f259c894d944f49
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-03-06 10:53:01 +0100

    feat: update auth api contract

--- libs/api/src/presentation/authentication/authRestApiContract.ts
+++ libs/api/src/presentation/authentication/authRestApiContract.ts
@@ -1,39 +1,57 @@
 import { initContract } from '@ts-rest/core';
 import { z } from 'zod';
-import { InternalServiceError, UnauthorizedError } from '../errors';
-import { loginPayload, LoginResponse, user } from './schemas';
+import {
+  BadRequestError,
+  InternalServiceError,
+  UnauthorizedError,
+} from '../errors';
+import { loginPayload, registerPayload, user } from './schemas';
 
 const c = initContract();
 
-export const authRestApiContract = c.router({
-  login: {
-    method: 'POST',
-    path: '/api/auth/login',
-    responses: {
-      200: LoginResponse,
-      401: UnauthorizedError,
-      500: InternalServiceError,
+export const authRestApiContract = c.router(
+  {
+    login: {
+      method: 'POST',
+      path: '/login',
+      responses: {
+        200: user,
+        401: UnauthorizedError,
+        500: InternalServiceError,
+      },
+      body: loginPayload,
+      summary: 'Log the user in',
     },
-    body: loginPayload,
-    summary: 'Log the user in',
-  },
-  me: {
-    method: 'GET',
-    path: '/api/auth/me',
-    responses: {
-      200: z.union([user, z.null()]),
-      500: InternalServiceError,
+    me: {
+      method: 'GET',
+      path: '/me',
+      responses: {
+        200: z.union([user, z.null()]),
+        500: InternalServiceError,
+      },
+      summary: 'Get the current user',
     },
-    summary: 'Get the current user',
-  },
-  logout: {
-    method: 'POST',
-    path: '/api/auth/logout',
-    responses: {
-      204: z.void(),
-      500: InternalServiceError,
+    logout: {
+      method: 'POST',
+      path: '/logout',
+      responses: {
+        204: z.void(),
+        500: InternalServiceError,
+      },
+      body: z.void(),
+      summary: 'Log the user out',
     },
-    body: z.void(),
-    summary: 'Log the user out',
+    register: {
+      method: 'POST',
+      path: '/register',
+      responses: {
+        200: user,
+        400: BadRequestError,
+        500: InternalServiceError,
+      },
+      body: registerPayload,
+      summary: 'Register a new user',
+    },
   },
-});
+  { pathPrefix: '/api/auth' }
+);
--- libs/api/src/presentation/authentication/schemas.ts
+++ libs/api/src/presentation/authentication/schemas.ts
@@ -1,9 +1,7 @@
 import * as z from 'zod';
 
 export const user = z.object({
-  id: z.string(),
   username: z.string(),
-  email: z.string().optional(),
   createdAt: z.date(),
 });
 
@@ -20,13 +18,12 @@ export const loginPayload = z.object({
   password: z.string(),
 });
 
-export const RefreshTokenResponse = z.object({
-  accessToken: z.string(),
-  refreshToken,
+export const registerPayload = z.object({
+  username: z.string(),
+  password: z.string(),
 });
 
-export const LoginResponse = z.object({
-  user,
+export const RefreshTokenResponse = z.object({
   accessToken: z.string(),
   refreshToken,
 });
@@ -37,5 +34,5 @@ export const LogoutResponse = z.object({
 
 export type User = z.infer<typeof user>;
 export type LoginPayload = z.infer<typeof loginPayload>;
-export type LoginResponse = z.infer<typeof LoginResponse>;
+export type RegisterPayload = z.infer<typeof registerPayload>;
 export type RefreshTokenResponse = z.infer<typeof RefreshTokenResponse>;
