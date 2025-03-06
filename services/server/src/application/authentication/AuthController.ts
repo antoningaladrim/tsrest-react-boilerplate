85c6944947e74a9d018d5a137bda2a6956b37290
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-03-06 10:52:05 +0100

    feat: update auth controller

--- services/server/src/application/authentication/AuthController.ts
+++ services/server/src/application/authentication/AuthController.ts
@@ -1,41 +1,66 @@
+import { clerkClient, User as ClerkUser, getAuth } from '@clerk/fastify';
 import type {
   LoginPayload,
-  LoginResponse,
-  RefreshTokenResponse,
+  RegisterPayload,
+  User,
 } from '@tsrest-react-boilerplate/api';
 import { FastifyRequest } from 'fastify';
-import { RefreshTokenRepository, UserRepository } from '../../domain';
-import { login, logout, me, refreshToken } from './methods';
+import { BadRequestError, UnauthorizedError } from '../../presentation/errors';
 
 export class AuthController {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly refreshTokenRepository: RefreshTokenRepository
-  ) {}
+  private formatClerkUser(clerkUser: ClerkUser): User {
+    return {
+      username: clerkUser.username,
+      createdAt: new Date(clerkUser.createdAt),
+    };
+  }
 
-  async login(props: LoginPayload): Promise<LoginResponse> {
-    return login({
-      props,
-      userRepository: this.userRepository,
-      refreshTokenRepository: this.refreshTokenRepository,
+  async login({ username, password }: LoginPayload): Promise<User> {
+    const {
+      data: [user],
+    } = await clerkClient.users.getUserList({ username: [username] });
+
+    if (!user) {
+      throw UnauthorizedError();
+    }
+
+    const { verified } = await clerkClient.users.verifyPassword({
+      userId: user.id,
+      password,
     });
+
+    if (!verified) {
+      throw UnauthorizedError();
+    }
+
+    return this.formatClerkUser(user);
   }
 
   async logout(request: FastifyRequest): Promise<void> {
-    return logout({
-      request,
-      refreshTokenRepository: this.refreshTokenRepository,
-    });
+    const { sessionId } = getAuth(request);
+    await clerkClient.sessions.revokeSession(sessionId);
   }
 
-  async me(request: FastifyRequest): Promise<LoginResponse['user']> {
-    return me({ request, userRepository: this.userRepository });
+  async me(request: FastifyRequest): Promise<User> {
+    const { userId } = getAuth(request);
+    const user = await clerkClient.users.getUser(userId);
+    return this.formatClerkUser(user);
   }
 
-  async refreshToken(token: string): Promise<RefreshTokenResponse> {
-    return refreshToken({
-      token,
-      refreshTokenRepository: this.refreshTokenRepository,
+  async register(body: RegisterPayload): Promise<User> {
+    const users = await clerkClient.users.getUserList({
+      username: [body.username],
     });
+
+    if (users.data.length > 0) {
+      throw BadRequestError();
+    }
+
+    const user = await clerkClient.users.createUser({
+      username: body.username,
+      password: body.password,
+    });
+
+    return this.formatClerkUser(user);
   }
 }
--- services/server/src/live.ts
+++ services/server/src/live.ts
@@ -1,12 +1,5 @@
 import { AuthController } from './application';
-import {
-  getRefreshTokenRepositoryLive,
-  getUserRepositoryLive,
-} from './repositoriesLive';
-
-const userRepository = getUserRepositoryLive();
-const tokenRepository = getRefreshTokenRepositoryLive();
 
 export const getAuthControllerLive = () => {
-  return new AuthController(userRepository, tokenRepository);
+  return new AuthController();
 };
