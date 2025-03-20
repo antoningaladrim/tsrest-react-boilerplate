10517acae021055b9c5a94f39461e850100dca24
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-03-20 09:31:33 +0100

    fix: init database script

--- infra/db/init-database.sh
+++ infra/db/init-database.sh
@@ -2,15 +2,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    DO \$\$
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB') THEN
             CREATE DATABASE "$POSTGRES_DB";
-            GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$POSTGRES_USER";
         END IF;
         
         IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'litellm') THEN
             CREATE DATABASE "litellm";
-            GRANT ALL PRIVILEGES ON DATABASE "litellm" TO "$POSTGRES_USER";
         END IF;
     END
+    \$\$;
+
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE "litellm" TO "$POSTGRES_USER";
 EOSQL
